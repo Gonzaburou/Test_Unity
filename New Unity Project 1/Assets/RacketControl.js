39ef7cb131c50221f0ce2a80248b3242c8ae4608
山本元柳斎権三郎 <panis@outlook.jp>
2015-06-15 16:39:22 +0900

    こんにちわ新しいUnity企画

---
+++ New Unity Project 1/Assets/RacketControl.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var Accel = 10000.0;
+function Update () {
+  rigidbody.AddForce(
+    transform.right * Input.GetAxisRaw( "Horizontal" ) * Accel,
+    ForceMode.Impulse
+  );
+}

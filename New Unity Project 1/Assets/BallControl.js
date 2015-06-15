57d8809f9a253843532d3100636236fec48f4d45
山本元柳斎権三郎 <panis@outlook.jp>
2015-06-15 18:17:10 +0900

    ボールの加速度いじった

--- New Unity Project 1/Assets/BallControl.js
+++ New Unity Project 1/Assets/BallControl.js
@@ -1,10 +1,15 @@
 ﻿#pragma strict
 
-var Speed = 20.0;
+var Speed = 2.0;
 function Start () {
 	rigidbody.AddForce(( transform.forward + transform.right ) * Speed,ForceMode.VelocityChange );
 }
 
 function Update () {
-
+	transform.position = new Vector3 (
+    	transform.position.x + 1 * Time.time/100,
+    	0,
+    	transform.position.z + 1 * Time.time/100
+    	);
+	Debug.Log(Time.time);
 }
